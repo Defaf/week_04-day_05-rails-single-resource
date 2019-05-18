1da0914299eb01065b911c8c8a9cf52d73d433d4
Defaf <defaf_kamfar@hotmail.com>
2019-05-18 03:22:27 +0300

    create first rails book app

---
+++ campus/app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController 
+
+    def index 
+        @books = Book.all
+    end 
+
+    def show
+        # puts params "will print the result in the consol"
+        @book =  Book.find(params[:id])
+    end
+    
+    def destroy
+        @book =  Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
+
+    def new 
+        @book = Book.new 
+    end 
+
+    def create 
+        @book = Book.create (book_params)
+        # redirect_to books_path 
+        redirect_to books_path(@book)
+    end 
+
+    def edit
+        @book = Book.find(params[:id])
+    end
+  
+    def update
+        @book = Book.find(params[:id])
+        @book.update(book_params)
+        redirect_to books_path(@book)
+    end
+  
+    def book_params
+        params.require(:book).permit(:title ,:author)
+    end
+end
+    
