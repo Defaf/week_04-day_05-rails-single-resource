a75112564c7c00c6f5dbb024b359842d103cc139
Defaf <defaf_kamfar@hotmail.com>
2019-05-20 12:04:42 +0300

    check for originalLanguage in schema

--- campus/db/schema.rb
+++ campus/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_084806) do
+ActiveRecord::Schema.define(version: 2019_05_20_085738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_084806) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "printDate"
+    t.string "originalLanguage"
   end
 
   create_table "welcomes", force: :cascade do |t|
