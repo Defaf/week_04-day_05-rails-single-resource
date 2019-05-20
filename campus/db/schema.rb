08959c5abe0c8a309cb12f3ab8af2560ff97568f
Defaf <defaf_kamfar@hotmail.com>
2019-05-20 11:55:23 +0300

    check schema for printDate

--- campus/db/schema.rb
+++ campus/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_082402) do
+ActiveRecord::Schema.define(version: 2019_05_20_084806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_082402) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "printDate"
   end
 
   create_table "welcomes", force: :cascade do |t|
