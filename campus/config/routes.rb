c0011c0abbb1f57357b4307545dc5470a86b6587
Defaf <defaf_kamfar@hotmail.com>
2019-05-20 11:29:13 +0300

    add welcome route

--- campus/config/routes.rb
+++ campus/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 
   get "/books", to: "books#index", as: "books" # book_path =/books
   get '/books/new', to: "books#new", as: "new_book"# book_path(book)=/books/new
@@ -11,4 +12,20 @@ Rails.application.routes.draw do
   post '/books', to: "books#create"
   patch '/books/:id', to: "books#update"
   
+  #author rout 
+  get "/authors", to: "authors#index", as: "authors" # author_path =/authors
+  get "/authors/new", to:"authors#new", as:"new_author"
+
+  get "/authors/:id", to: "authors#show", as: "author" # author_path(author)=/authors/:id
+
+  delete "/authors/:id", to:"authors#destroy"
+
+  post "/authors", to:"authors#create"
+
+  get '/authors/:id/edit', to: "authors#edit",as: "edit_author"
+  patch '/authors/:id', to: "authors#update"
+
+  root 'welcome#index'
+
 end
+
