69600a669acfd18ae30ed1bbb9b35dac304f1a47
Defaf <defaf_kamfar@hotmail.com>
2019-05-21 13:11:50 +0300

    create harry potter rails app

---
+++ harryPotter/db/migrate/20190521101030_create_houses.rb
@@ -0,0 +1,12 @@
+class CreateHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.string :animal
+      t.string :diagnosis
+      t.string :slogan
+
+      t.timestamps
+    end
+  end
+end
